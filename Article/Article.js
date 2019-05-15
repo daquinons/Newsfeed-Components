de050b513ec22855f532bb4febc8429854a18ad1
David Quiñones <daquinons@users.noreply.github.com>
2019-05-15 13:45:17 +0200

    Add Article opening animation

--- Article/Article.js
+++ Article/Article.js
@@ -1,5 +1,8 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
+const expandText = 'Click to Expand';
+const closeText = 'Click to Close';
+
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
@@ -7,16 +10,22 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    this.expandButton.textContent = expandText;
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
-    
+    //this.domElement.classList.toggle('article-open');
 
+    if ($(this.domElement).height() === 50) {
+      $(this.domElement).animate({height: 400}, 200);
+      this.expandButton.textContent = closeText;
+    } else {
+      $(this.domElement).animate({height: 50}, 200);
+      this.expandButton.textContent = expandText;
+    }
   }
 }
 
